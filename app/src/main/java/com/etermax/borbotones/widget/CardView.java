48ee5a2d9eb17c97a6f0b6b6735b056e72c4cce0
Hernan Bosqued <hernan.bosqued@etermax.com>
2016-09-16 15:06:17 -0300

    CardView click

--- app/src/main/java/com/etermax/borbotones/widget/CardView.java
+++ app/src/main/java/com/etermax/borbotones/widget/CardView.java
@@ -2,10 +2,11 @@ package com.etermax.borbotones.widget;
 
 import android.content.Context;
 import android.view.View;
+import android.widget.Toast;
 
 import com.etermax.borbotones.model.Card;
 
-public class CardView extends View {
+public class CardView extends View implements View.OnClickListener{
 
     Card mCard;
 
@@ -13,10 +14,16 @@ public class CardView extends View {
         super(context);
         mCard = card;
         setBackgroundColor( mCard.mType.getColor() );
+        setOnClickListener(this);
     }
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec > 0 ? heightMeasureSpec : widthMeasureSpec);//Square
+    }
+
+    @Override
+    public void onClick(View v) {
+        Toast.makeText(getContext(), "CLICK", Toast.LENGTH_SHORT).show();
     }
 }
