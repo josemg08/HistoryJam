9d98e1f5df9cf7e1a660c59d3f498d5124e7705a
jose.gonzalez <jose.gonzalez@etermax.com>
2016-09-16 13:10:05 -0300

    Add historicEventSelection screen

--- app/src/main/java/com/etermax/borbotones/ModeSelectionActivity.java
+++ app/src/main/java/com/etermax/borbotones/ModeSelectionActivity.java
@@ -2,6 +2,8 @@ package com.etermax.borbotones;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * .___
@@ -9,13 +11,29 @@ import android.support.v7.app.AppCompatActivity;
  * __.
  */
 
-public class ModeSelectionActivity extends AppCompatActivity {
+public class ModeSelectionActivity extends AppCompatActivity implements Button.OnClickListener {
+
+    private Button versusButton;
+    private Button historyButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mode_selection_layout);
+        versusButton = (Button) findViewById(R.id.vs_button);
+        versusButton.setOnClickListener(this);
+        historyButton = (Button) findViewById(R.id.history_button);
+        historyButton.setOnClickListener(this);
     }
 
+    @Override
+    public void onClick(View view) {
+        switch(view.getId()){
+            case R.id.vs_button:
+                break;
+            case R.id.history_button:
+                break;
+        }
+    }
 }
 //.___ End of ModeSelectionActivity __./
