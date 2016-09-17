10e86efde1eec96c6bce6560a91f37e52893c8d9
Carlos Sanchez <carlos.sanchez@etermax.com>
2016-09-17 14:21:54 -0300

    working cardholder

--- app/src/main/java/com/etermax/borbotones/ui/CardPlayedHolder.java
+++ app/src/main/java/com/etermax/borbotones/ui/CardPlayedHolder.java
@@ -3,6 +3,7 @@ package com.etermax.borbotones.ui;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -18,13 +19,15 @@ import com.etermax.borbotones.model.Card;
  * __.
  */
 
-class CardPlayedHolder extends RelativeLayout{
+class CardPlayedHolder extends RelativeLayout implements View.OnClickListener{
 
     Context context;
     private ImageView cardPlayedHolderImage;
     private TextView cardPlayedHolderText;
     LayoutInflater inflater;
     Card card;
+    View selectorView;
+    private boolean selected = false;
 
     public CardPlayedHolder(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
@@ -48,6 +51,9 @@ class CardPlayedHolder extends RelativeLayout{
         LayoutInflater.from(context).inflate(R.layout.card_played_holder, this);
         cardPlayedHolderText =(TextView) findViewById(R.id.card_played_holder_text);
         cardPlayedHolderImage = (ImageView) findViewById(R.id.card_played_holder_image);
+        selectorView = findViewById(R.id.card_selector_view);
+        selectorView.setVisibility(GONE);
+        setOnClickListener(this);
     }
 
     public void setCard(Card card){
@@ -64,6 +70,8 @@ class CardPlayedHolder extends RelativeLayout{
         Card card = this.card;
         this.card = null;
         cardPlayedHolderImage.setImageDrawable(getResources().getDrawable(R.drawable.placeholder));
+        selected = false;
+        selectorView.setVisibility(GONE);
         return card;
     }
 
@@ -74,6 +82,20 @@ class CardPlayedHolder extends RelativeLayout{
     public void setEmptySloth(){
         card = null;
         cardPlayedHolderImage.setImageDrawable(getResources().getDrawable(R.drawable.placeholder));
+    }
+
+
+
+    @Override
+    public void onClick(View v) {
+        if(card !=null && !selected){
+            selected = true;
+            selectorView.setVisibility(VISIBLE);
+        }
+    }
+
+    public interface OnCardHolderListener{
+
     }
 
 }
